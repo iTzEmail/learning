05cea3308702045aec01dbadedb509eb50f81d65
iTzEmail <lyric8utube@gmail.com>
2025-10-14 20:43:24 -0400

    hey

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello from index.js");
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYCId6a0xOqbDi13rko_bSvIRv0KNmcNI",
+  authDomain: "fir-9-26d77.firebaseapp.com",
+  projectId: "fir-9-26d77",
+  storageBucket: "fir-9-26d77.firebasestorage.app",
+  messagingSenderId: "995514548945",
+  appId: "1:995514548945:web:a43c7592b043b164f17bf0",
+  measurementId: "G-BPNQ5G1XH0"
+};
+
+initializeApp(firebaseConfig);
